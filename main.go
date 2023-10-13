38990483dc96e6de5636ce404b0b7ba835c34357
highashell <220100@astanait.edu.kz>
2023-10-13 12:41:10 +0600

    change logic of singleton

--- main.go
+++ main.go
@@ -2,36 +2,36 @@ package main
 
 import (
 	"fmt"
+	"strategy-as1/config"
 	"strategy-as1/soldier_strategy"
 )
 
 func main() {
 	//singleton
-	soldier := soldier_strategy.GetBasicSoldier()
-	singletonSoldier := soldier_strategy.GetBasicSoldier()
-	basicSoldier := soldier
-	BasicSoldier1 := singletonSoldier
+	instanceOfDB := config.GetInstanceOfDB()
+	instanceOfDB.Info()
+	configSingleton := config.GetInstanceOfDB()
+	configSingleton.Info()
 	//strategy/decorator
-	bowSoldier := soldier_strategy.BowSoldier{Soldier: basicSoldier}
-	shieldSoldier := soldier_strategy.ShieldSoldier{Soldier: basicSoldier}
+	basicSoldier := soldier_strategy.BasicSoldier{}
+	bowSoldier := soldier_strategy.BowSoldier{Soldier: &basicSoldier}
+	shieldSoldier := soldier_strategy.ShieldSoldier{Soldier: &basicSoldier}
 	shieldBowSoldier := soldier_strategy.ShieldSoldier{
 		Soldier: &soldier_strategy.BowSoldier{
-			Soldier: basicSoldier,
+			Soldier: &basicSoldier,
 		},
 	}
 	fmt.Println()
 
-	soldier1 := soldier_strategy.SoldierBehavior{SB: basicSoldier}
+	soldier1 := soldier_strategy.SoldierBehavior{SB: &basicSoldier}
 	soldier2 := soldier_strategy.SoldierBehavior{SB: &bowSoldier}
 	soldier3 := soldier_strategy.SoldierBehavior{SB: &shieldSoldier}
 	soldier4 := soldier_strategy.SoldierBehavior{SB: &shieldBowSoldier}
-	soldier5 := soldier_strategy.SoldierBehavior{SB: BasicSoldier1}
 
 	soldier1.DisplayStats()
 	soldier2.DisplayStats()
 	soldier3.DisplayStats()
 	soldier4.DisplayStats()
-	soldier5.DisplayStats()
 
 	//adapter
 	fmt.Println()
